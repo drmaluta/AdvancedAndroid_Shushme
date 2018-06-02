d60d3ed895704b681503555d12bc3425ed1ef566
drmaluta <admin@bardsong.com>
2018-06-02 15:54:16 -0700

    Completed all TODO tasks

--- app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -16,10 +16,22 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -35,14 +47,105 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "onReceive called");
         // TODO (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            Log.e(TAG, String.format("Error code : %d", geofencingEvent.getErrorCode()));
+            return;
+        }
 
         // TODO (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
         // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+        // Check which transition type has triggered this event
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            setRingerMode(context, AudioManager.RINGER_MODE_SILENT);
+        } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            setRingerMode(context, AudioManager.RINGER_MODE_NORMAL);
+        } else {
+            // Log the error.
+            Log.e(TAG, String.format("Unknown transition : %d", geofenceTransition));
+            // No need to do anything else
+            return;
+        }
 
         // TODO (6) Show a notification to alert the user that the ringer mode has changed.
         // Feel free to create a helper method (sendNotification)
+        // Send the notification
+        sendNotification(context, geofenceTransition);
 
+    }
+
+    private void sendNotification(Context context, int transitionType) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        // Get a notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+
+        // Check the transition type to display the relevant icon image
+        if (transitionType == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            builder.setSmallIcon(R.drawable.ic_volume_off_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_off_white_24dp))
+                    .setContentTitle(context.getString(R.string.silent_mode_activated));
+        } else if (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            builder.setSmallIcon(R.drawable.ic_volume_up_white_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.ic_volume_up_white_24dp))
+                    .setContentTitle(context.getString(R.string.back_to_normal));
+        }
+
+        // Continue building the notification
+        builder.setContentText(context.getString(R.string.touch_to_relaunch));
+        builder.setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
+    private void setRingerMode(Context context, int mode) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // Check for DND permissions for API 24+
+        if (android.os.Build.VERSION.SDK_INT < 24 ||
+                (android.os.Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted())) {
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(mode);
+        }
     }
 }
